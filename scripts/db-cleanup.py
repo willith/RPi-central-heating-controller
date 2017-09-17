4e8367fb03938154baff6ead9e489b14b7bae4cb
willith <31216749+willith@users.noreply.github.com>
2017-09-17 17:51:33 +0100

    Create db-cleanup.py

---
+++ scripts/db-cleanup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from shutil import copyfile
+import sqlite3
+from datetime import datetime, date
+import dateutil.relativedelta
+now = date.today()
+date = now + dateutil.relativedelta.relativedelta(months=-1)
+copyfile("/home/pi/database/centralheating.db", "/home/pi/database/archive/centralheating-%s.db" % date)
+
+conn=sqlite3.connect('/home/pi/database/centralheating.db')
+
+curs=conn.cursor()
+
+curs.execute("DELETE FROM temps WHERE tdate >= date('now', 'start of month', '-1 month') AND tdate < date('now', 'start of month')")
+conn.commit()
+
+	
+
+
+conn.close()
